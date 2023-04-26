9363426a045d31d68083ab2a8a06e5f58df7146f
JhonathanAlejandro01 <jhonathanalejandroa01@gmail.com>
2023-04-25 20:40:36 -0500

    exercise 2

---
+++ cambia numeros/numeroSeleccionado.php
@@ -0,0 +1,31 @@
+﻿<!DOCTYPE html>
+<html>
+
+<head>
+    <title>Unidad 4 - Ejercicio 2 Seleccionar N&uacute;mero</title>
+    <meta http-equiv="Content-Type" content="text/html; charset=ISO-8859-1" />
+</head>
+
+<body>
+    <?php
+        /* En este programa se va a pintar en pantalla
+         * la selecion dle numero en el archivo selecionarNumero.php
+         * y se hacen las respectivas validadaciones
+         * y se le coloca le cambia el color del texto a rojo
+         * por medio del dato obtenido
+         */
+        if (isset($_GET['numeroSeleccionado'])) {
+            $numeroSeleccionado = $_GET['numeroSeleccionado'];
+            $color = $_GET['color'];
+            echo "<p style='color:$color;'>Usted ha seleccionado el n&uacute;mero: $numeroSeleccionado"
+                    . "<br />";
+            unset($_GET);
+            echo "<a href = 'seleccionarNumero.php'>Volver al inicio</a>";
+        } else {
+            echo "<h3>Por favor seleccione uno de los n&uacute;meros</h3>";
+            echo "<a href = 'seleccionarNumero.php'>Volver al inicio</a>";
+        }
+        ?>
+</body>
+
+</html>
---
+++ cambia numeros/seleccionarNumero.php
@@ -0,0 +1,29 @@
+﻿<!DOCTYPE html>
+<html>
+
+<head>
+    <title>Unidad 4 - Ejercicio 2 Seleccionar N&uacute;mero</title>
+    <meta http-equiv="Content-Type" content="text/html; charset=ISO-8859-1" />
+</head>
+
+<body>
+    <?php
+        /* En este programa también se van a usar hipervínculos para enviar
+         * información a la misma página, pero en este caso los hipervínculos se
+         * van  a construir dinámicamente usando un ciclo for
+         */
+            echo "<h3>Por favor seleccione uno de los n&uacute;meros</h3>";
+            for ($i = 1; $i <= 10; $i++) {
+                /* Como puede verse se usa el índice del ciclo for para
+                 * construir los hipervínculos de forma dinámica se van a crear
+                 * diez hipervínculos cada uno con un valor numérico diferente 
+                 * que se transmiete en la variable numeroSeleccionado
+                 * y con el color=red
+                 */
+                echo "<a href = 'numeroSeleccionado.php?numeroSeleccionado=$i&color=red'>"
+                . "$i</a><br />";
+            }
+        ?>
+</body>
+
+</html>
